fa1df43dc60385489a869c9baf1c0c5ca3ff651d
Ningyuan Li <mariotaku.lee@gmail.com>
2025-02-25 02:02:28 +0900

    LG WebOS: RPATH fix

--- os/webos/package.sh
+++ os/webos/package.sh
@@ -12,19 +12,12 @@ cp -r ../../build/openttd dist/openttd
 cp -r public/. dist/
 
 mkdir dist/lib
-cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libicudata.so.70.1 dist/lib
-ln -rs dist/lib/libicudata.so.70.1 dist/lib/libicudata.so.70
-cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libicui18n.so.70.1 dist/lib
-ln -rs dist/lib/libicui18n.so.70.1 dist/lib/libicui18n.so.70
-cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libicuuc.so.70.1 dist/lib
-ln -rs dist/lib/libicuuc.so.70.1 dist/lib/libicuuc.so.70
-cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libstdc++.so.6.0.30 dist/lib
-ln -rs dist/lib/libstdc++.so.6.0.30 dist/lib/libstdc++.so.6
-cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libfluidsynth.so.3.3.3 dist/lib
-ln -rs dist/lib/libfluidsynth.so.3.3.3 dist/lib/libfluidsynth.so.3
-cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libreadline.so.8.1 dist/lib
-ln -rs dist/lib/libreadline.so.8.1 dist/lib/libreadline.so.8
-cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/lib/libatomic.so.1.2.0 dist/lib
-ln -rs dist/lib/libatomic.so.1.2.0 dist/lib/libatomic.so.1
+cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libicudata.so.70.1 dist/libicudata.so.70
+cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libicui18n.so.70.1 dist/libicui18n.so.70
+cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libicuuc.so.70.1 dist/libicuuc.so.70
+cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libstdc++.so.6.0.30 dist/libstdc++.so.6
+cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libfluidsynth.so.3.3.3 dist/libfluidsynth.so.3
+cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/usr/lib/libreadline.so.8.1 dist/libreadline.so.8
+cp $TOOLCHAIN_DIRECTORY/arm-webos-linux-gnueabi/sysroot/lib/libatomic.so.1.2.0 dist/libatomic.so.1
 
 ares-package dist/
